77e3c3790ecd537154122a47f210b1cfe6ee01c5
Steve Elliott <steve@tegud.net>
2017-06-26 20:02:56 +0100

    faces too

--- handler.js
+++ handler.js
@@ -1,24 +1,54 @@
 const AWS = require('aws-sdk');
 const rek = new AWS.Rekognition();
 
-module.exports.info = (event, context, callback) => {
-    rek.detectLabels({
+function getLabels(bucket, image) {
+    return new Promise((resolve, reject) => rek.detectLabels({
         Image: {
             S3Object: {
-                Bucket: "rekognition-tegud-test",
-                Name: "19424023_10102800025414035_3309316934660864874_n.jpg",
+                Bucket: bucket,
+                Name: image
             },
         },
         MaxLabels: 10,
         MinConfidence: 50
     }, (err, data) => {
         if (err) {
-            return callback(err);
+            return reject(err);
         }
 
+        resolve(data);
+    ));
+}
+
+function getFaces(bucket, image) {
+    return new Promise((resolve, reject) => rek.detectFaces({
+        Image: {
+            S3Object: {
+                Bucket: bucket,
+                Name: image
+            },
+        }
+    }, (err, data) => {
+        if (err) {
+            return reject(err);
+        }
+
+        resolve(data);
+    ));
+}
+
+module.exports.info = (event, context, callback) => {
+    Promise.all([
+        getLabels('rekognition-tegud-test', '19424023_10102800025414035_3309316934660864874_n.jpg'),
+        getFaces('rekognition-tegud-test', '19424023_10102800025414035_3309316934660864874_n.jpg')
+    ])
+    .then(data => {
         callback(null, {
             statusCode: 200,
-            body: JSON.stringify(data),
+            body: JSON.stringify({
+                labels: data[0],
+                faces: data[1]
+            }),
         });
     });
 };
