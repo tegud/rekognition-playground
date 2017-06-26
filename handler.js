e55de02bd9c287aae3f47745078d578da0ff2475
Steve Elliott <steve@tegud.net>
2017-06-26 14:48:30 +0100

    test

---
+++ handler.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk');
+const rek = new AWS.Rekognition();
+
+module.exports.hello = (event, context, callback) => {
+    rek.detectLabels({
+        Image: {
+            S3Object: {
+                Bucket: "rekognition-tegud-test",
+                Name: "19424023_10102800025414035_3309316934660864874_n.jpg",
+            },
+        },
+        MaxLabels: 10,
+        MinConfidence: 50
+    }, (err, data) => {
+        if (err) {
+            return callback(err);
+        }
+
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({}),
+        });
+    });
+};
